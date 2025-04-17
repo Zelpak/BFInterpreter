bd4ed882f3a8a534eaf03a44d698bb133d7ee939
Zelpak <chkirid6@gmail.com>
2025-04-17 23:04:50 +0100

    Create Interpreter.py

---
+++ Interpreter.py
@@ -0,0 +1,43 @@
+def Exc(code):
+    cells = [0] * 30000
+    ptr = 0
+    i = 0
+    loop_stack = []
+
+    while i < len(code):
+        cmd = code[i]
+
+        if cmd == '>':
+            ptr += 1
+        elif cmd == '<':
+            ptr -= 1
+        elif cmd == '+':
+            cells[ptr] = (cells[ptr] + 1) % 256
+        elif cmd == '-':
+            cells[ptr] = (cells[ptr] - 1) % 256
+        elif cmd == '.':
+            print(chr(cells[ptr]), end='')
+        elif cmd == ',':
+            cells[ptr] = ord(input("Input a character: ")[0])
+        elif cmd == '[':
+            if cells[ptr] == 0:
+                open_brackets = 1
+                while open_brackets:
+                    i += 1
+                    if code[i] == '[':
+                        open_brackets += 1
+                    elif code[i] == ']':
+                        open_brackets -= 1
+            else:
+                loop_stack.append(i)
+        elif cmd == ']':
+            if cells[ptr] != 0:
+                i = loop_stack[-1]
+                continue
+            else:
+                loop_stack.pop()
+
+        i += 1
+
+with open("main.b", "r") as f: # set main.b as your code's name
+    Exc(f.read())
